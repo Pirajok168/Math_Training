c843b94044c20a0181f1c798d5f02c7cbfb344ea
danilaeremin <danilka.eremin@gmail.com>
2022-03-27 14:20:03 +0800

    Небольшая настройка профиля

--- app/src/main/java/com/example/mathtraining/screens/Profile.kt
+++ app/src/main/java/com/example/mathtraining/screens/Profile.kt
@@ -4,34 +4,54 @@ import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.mathtraining.R
 import com.example.mathtraining.itemWorkpiece.CardInfoOrSetting
 import com.example.mathtraining.nav.Screens
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun Profile(navHostController: NavHostController) {
@@ -98,7 +118,7 @@ fun Header(date: String) {
                    verticalArrangement = Arrangement.Center
                ) {
                    Text(text = stringResource(id = R.string.joined)
-                       , color = Color.Gray
+                       , color = Color(0xFF745B96)
                        , fontWeight = FontWeight.Bold)
 
                    Text(buildAnnotatedString {
@@ -126,17 +146,99 @@ fun Body(navHostController: NavHostController) {
     SettingsAndInfo(navHostController)
 }
 
+
+@OptIn(ExperimentalMaterialApi::class, androidx.compose.ui.ExperimentalComposeUiApi::class)
 @Composable
-fun FIO(name: String, surname: String) {
+fun FIO(_name: String, _surname: String
+        , lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+) {
+    var enable by remember {
+        mutableStateOf(false)
+    }
+
+    var name by remember {
+        mutableStateOf(_name)
+    }
+
+    var surname by remember {
+        mutableStateOf(_surname)
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+    val requester = FocusRequester()
+    val scope = rememberCoroutineScope()
+
 
     Column(modifier = Modifier.padding(horizontal = 28.dp, vertical = 15.dp)) {
-        Text(text = name
-            , fontWeight = FontWeight.ExtraBold
-            , fontSize = 50.sp
-            , color = Color(0xFF745B96)
+
+        BadgeBox(
+            backgroundColor= Color.Transparent,
+            badgeContent = {
+                IconButton(onClick = {
+                    if (enable) {
+                        focusManager.clearFocus()
+                        enable = false
+                        keyboardController?.hide()
+                    }else{
+                        scope.launch {
+                            enable = true
+                            delay(1000L)
+                            requester.requestFocus()
+                            keyboardController?.show()
+                        }
+                    }
+
+
+                }) {
+                    Icon(imageVector = if (enable) Icons.Default.Done else Icons.Default.Edit
+                        , contentDescription = "", tint = if (enable) Color(0xFF745B96) else Color.Black)
+                }
+
+            }
+        ) {
+
+            BasicTextField(value =name, onValueChange = { name=it},
+                textStyle = TextStyle(color = if(enable) Color.Black else Color(0xFF745B96),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.ExtraBold),
+                enabled =  enable,
+                modifier = Modifier
+                    .requiredWidth(250.dp)
+                    .focusRequester(requester),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    })
+            )
+
+
+
+        }
+
+        BasicTextField(value =surname, onValueChange = { surname=it},
+            textStyle = TextStyle(color = if(enable) Color.Black else Color(0xFF9E8FB2),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Medium),
+            enabled =  enable,
+            modifier = Modifier
+                .requiredWidth(250.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                    focusManager.clearFocus()
+                    enable = false
+                })
         )
-        
-        Text(text = surname, fontWeight = FontWeight.Medium, color =  Color(0xFF9E8FB2), fontSize = 40.sp)
+
+    }
+    DisposableEffect(lifecycleOwner){
+        onDispose {
+            keyboardController?.hide()
+            focusManager.clearFocus()
+            enable = false
+        }
     }
 
 }
