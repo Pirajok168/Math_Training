134a448847cb952e796ad7d900d8cbb00ae60618
Danila <danilka.eremin@gmail.com>
2022-03-30 00:07:06 +0800

    Уведомленияпум

--- app/src/main/java/com/example/mathtraining/PushApp.kt
+++ app/src/main/java/com/example/mathtraining/PushApp.kt
@@ -1,11 +1,19 @@
 package com.example.mathtraining
 
 import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.util.Log
+import android.widget.Toast
+import com.example.mathtraining.notifications.PushService
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
 class PushApp: Application() {
+
+    private lateinit var pushBroadcast: BroadcastReceiver
 
     override fun onCreate() {
         super.onCreate()
@@ -20,5 +28,35 @@ class PushApp: Application() {
             val token = task.result
             Log.e("token", "Token - $token")
         }
+
+        pushBroadcast = object : BroadcastReceiver(){
+            override fun onReceive(context:  Context?, intent: Intent?) {
+                val extras =intent?.extras
+                extras?.keySet()?.firstOrNull {
+                    it == PushService.KEY_ACTION
+                }?.let {
+                        key ->
+                    when(extras.getString(key)){
+                        PushService.ACTION_SHOW_MESSAGE ->{
+
+                            extras.getString(PushService.KEY_MESSAGE)?.let {
+                                Log.e("token", "Сообщение - $it")
+                                Toast.makeText(applicationContext, it, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        else -> {
+                            Toast.makeText(applicationContext, "Ничего нет", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            }
+
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(PushService.INTENT_FILTER)
+        registerReceiver(pushBroadcast, intentFilter )
     }
+
+
 }
--- app/src/main/java/com/example/mathtraining/notifications/PushService.kt
+++ app/src/main/java/com/example/mathtraining/notifications/PushService.kt
@@ -1,11 +1,18 @@
 package com.example.mathtraining.notifications
 
-import android.content.BroadcastReceiver
-import android.content.Context
+import android.R
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
 import android.util.Log
+import androidx.compose.ui.graphics.Color
+import androidx.core.app.NotificationCompat
+import com.example.mathtraining.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
 
 class PushService: FirebaseMessagingService() {
 
@@ -19,7 +26,22 @@ class PushService: FirebaseMessagingService() {
 
 
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        Log.e("token", "тест - ${message.from}")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val intent = Intent(INTENT_FILTER)
+        remoteMessage.data.forEach{
+                entry ->
+            intent.putExtra(entry.key, entry.value)
+        }
+
+        sendBroadcast(intent)
+    }
+
+
+
+    companion object{
+        const val INTENT_FILTER = "PUSH_EVENT"
+        const val ACTION_SHOW_MESSAGE = "show_message"
+        const val KEY_MESSAGE = "message"
+        const val KEY_ACTION = "action"
     }
 }
